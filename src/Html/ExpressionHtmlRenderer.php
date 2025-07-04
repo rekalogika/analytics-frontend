af5c6589400baf256f55e7c76842101c60e385b0
root <1102197+priyadi@users.noreply.github.com>
2025-07-04 09:21:30 +0000

    feat: show current active criteria in html output (#401)

---
+++ src/Html/ExpressionHtmlRenderer.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of rekalogika/analytics package.
+ *
+ * (c) Priyadi Iman Nurcahyo <https://rekalogika.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
+namespace Rekalogika\Analytics\Frontend\Html;
+
+use Rekalogika\Analytics\Common\Exception\LogicException;
+use Rekalogika\Analytics\Contracts\Query;
+use Rekalogika\Analytics\Frontend\Formatter\Htmlifier;
+use Rekalogika\Analytics\Frontend\Html\Visitor\HtmlRendererExpressionVisitor;
+use Rekalogika\Analytics\Metadata\Summary\SummaryMetadataFactory;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+final readonly class ExpressionHtmlRenderer
+{
+    public function __construct(
+        private Htmlifier $htmlifier,
+        private SummaryMetadataFactory $summaryMetadataFactory,
+        private TranslatorInterface $translator,
+    ) {}
+
+    /**
+     * @return list<string>
+     */
+    public function renderExpression(Query $query): array
+    {
+        $summaryMetadata = $this->summaryMetadataFactory
+            ->getSummaryMetadata($query->getFrom());
+
+        $visitor = new HtmlRendererExpressionVisitor(
+            htmlifier: $this->htmlifier,
+            summaryMetadata: $summaryMetadata,
+            translator: $this->translator,
+        );
+
+        $expressions = $query->getWhere();
+
+        $results = [];
+
+        foreach ($expressions as $expression) {
+            $result = $visitor->dispatch($expression);
+
+            if (!\is_string($result)) {
+                throw new LogicException('Expected string result from expression rendering, got: ' . get_debug_type($result));
+            }
+
+            $results[] = $result;
+        }
+
+        return $results;
+    }
+}
---
+++ src/Html/Visitor/HtmlRendererExpressionVisitor.php
@@ -0,0 +1,138 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of rekalogika/analytics package.
+ *
+ * (c) Priyadi Iman Nurcahyo <https://rekalogika.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
+namespace Rekalogika\Analytics\Frontend\Html\Visitor;
+
+use Doctrine\Common\Collections\Expr\Comparison;
+use Doctrine\Common\Collections\Expr\CompositeExpression;
+use Doctrine\Common\Collections\Expr\ExpressionVisitor;
+use Doctrine\Common\Collections\Expr\Value;
+use Rekalogika\Analytics\Common\Exception\LogicException;
+use Rekalogika\Analytics\Common\Model\TranslatableMessage;
+use Rekalogika\Analytics\Frontend\Formatter\Htmlifier;
+use Rekalogika\Analytics\Metadata\Summary\SummaryMetadata;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+/**
+ * @api
+ */
+class HtmlRendererExpressionVisitor extends ExpressionVisitor
+{
+    public function __construct(
+        private Htmlifier $htmlifier,
+        private SummaryMetadata $summaryMetadata,
+        private TranslatorInterface $translator,
+    ) {}
+
+    #[\Override]
+    public function walkComparison(Comparison $comparison): string
+    {
+        return \sprintf(
+            '%s %s %s',
+            $this->walkDimension($comparison->getField()),
+            $this->walkOperator($comparison->getOperator()),
+            $this->walkValue($comparison->getValue()),
+        );
+    }
+
+    #[\Override]
+    public function walkValue(Value $value): string
+    {
+        /** @psalm-suppress MixedAssignment */
+        $value = $value->getValue();
+
+        if (\is_array($value)) {
+            $parts = [];
+
+            /** @psalm-suppress MixedAssignment */
+            foreach ($value as $part) {
+                $parts[] = $this->walkValue(new Value($part));
+            }
+
+            return '(' . implode(', ', $parts) . ')';
+        }
+
+        return $this->htmlifier->toHtml($value);
+    }
+
+    #[\Override]
+    public function walkCompositeExpression(CompositeExpression $expr): string
+    {
+        $parts = [];
+
+        foreach ($expr->getExpressionList() as $part) {
+            /** @psalm-suppress MixedAssignment */
+            $string = $this->dispatch($part);
+
+            if (!\is_string($string)) {
+                throw new LogicException('Expected string from expression dispatch, got ' . \gettype($string));
+            }
+
+            $parts[] = $string;
+        }
+
+        if ($expr->getType() === CompositeExpression::TYPE_NOT) {
+            return \sprintf(
+                '%s %s',
+                $this->walkCompositeExpressionType($expr->getType()),
+                implode(' ', $parts),
+            );
+        }
+
+        if (\count($parts) === 1) {
+            return $parts[0];
+        }
+
+        $type = $this->walkCompositeExpressionType($expr->getType());
+
+        return '(' . implode(' ' . $type . ' ', $parts) . ')';
+    }
+
+    protected function walkOperator(string $operator): string
+    {
+        return match ($operator) {
+            Comparison::EQ => '=',
+            Comparison::NEQ => '≠',
+            Comparison::LT => '<',
+            Comparison::LTE => '≤',
+            Comparison::GT => '>',
+            Comparison::GTE => '≥',
+            Comparison::IN => '∈',
+            Comparison::NIN => '∉',
+            default => throw new LogicException('Unsupported operator: ' . $operator),
+        };
+    }
+
+    protected function walkCompositeExpressionType(string $type): string
+    {
+        $translatable = match ($type) {
+            CompositeExpression::TYPE_AND => new TranslatableMessage('AND'),
+            CompositeExpression::TYPE_OR => new TranslatableMessage('OR'),
+            CompositeExpression::TYPE_NOT => new TranslatableMessage('NOT'),
+            default => throw new LogicException('Unsupported composite expression type: ' . $type),
+        };
+
+        return htmlspecialchars($translatable->trans($this->translator));
+    }
+
+    protected function walkDimension(string $field): string
+    {
+        $dimension = $this->summaryMetadata->getDimension($field);
+        $string = $this->htmlifier->toHtml($dimension->getLabel()->getRootAndLeaf());
+
+        return \sprintf(
+            '<u>%s</u>',
+            $string,
+        );
+    }
+}
